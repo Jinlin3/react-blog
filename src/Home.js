4862b54725e180ead927c5d3554fa4cd6ec01d7e
JJ PC <hellojjlin@gmail.com>
2023-08-22 16:47:39 -0400

    Outputting lists

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,19 @@
 import { useState } from 'react';
 
 const Home = () => {
-  
-  const [name, setName] = useState('mario');
-  const [age, setAge] = useState(25);
-
-  const handleClick = (e) => {
-    if (name === 'mario') {
-      setName('luigi');
-      setAge(30);
-    } else {
-      setName('mario');
-      setAge(25);
-    }
-  }
-
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ]);
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>{ name } is { age } years old</p>
-      <button onClick={handleClick}>Click me</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+        </div>
+      ))}
     </div>
   );
 }
@@ -27,3 +21,6 @@ const Home = () => {
 export default Home;
 
 // Use an anonymous function to invoke functions with parameters
+
+// .map cycles through arrays
+// key property should be used when creating lists in React to distinugish them from each other
