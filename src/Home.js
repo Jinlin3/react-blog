71618f3a6769f25bbb76f50c01266093fa736428
JJ PC <hellojjlin@gmail.com>
2023-08-23 04:30:50 -0400

    Reusing Components

--- src/Home.js
+++ src/Home.js
@@ -9,7 +9,8 @@ const Home = () => {
   ]);
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs!" />
+      <BlogList blogs={blogs} title="All Blogs" />
+      <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs" />
     </div>
   );
 }
@@ -18,8 +19,10 @@ export default Home;
 
 // Use an anonymous function to invoke functions with parameters
 
-// .map cycles through arrays
+// (callback function) .map cycles through arrays
 
 // key property should be used when creating lists in React to distinugish them from each other
 
 // props allow for the passing of data from a parent component to a child component
+
+// (callback function) .filter returns a new array depending on the boolean values of each item
